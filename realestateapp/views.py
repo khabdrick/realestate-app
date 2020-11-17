ad0c35ca189f937f5dbf07cd477fba41ac4b8ca9
khabdrick <muhamzyali@gmail.com>
2020-11-17 20:26:20 +0100

    made some changes to the index page

--- realestateapp/views.py
+++ realestateapp/views.py
@@ -1,21 +1,17 @@
 from django.contrib.auth import authenticate, login, get_user_model
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 from properties.models import Property
 
 
 def home_page(request):
-	object_list = Property.objects.all().featured()
-	context 	= {
-		"object_list": object_list,
-	}
-	return render(request, 'index.html', context)
-    
+    object_list = Property.objects.all().featured()
+    context = {
+        "object_list": object_list,
+    }
+    return render(request, 'index.html', context)
+
 
 def about_page(request):
     return render(request, "about.html")
-
-
-
-
